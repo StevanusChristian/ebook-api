3d1d1f7373e8d0ae6aab70b5a2013dcc486b6518
Re: Stristy <3103118128@student.smktelkom-pwt.sch.id>
2020-10-02 08:54:18 +0700

    JWT Auth

---
+++ routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+/*
+Route::get("books", "BookController@index");
+Route::post("books", "BookController@store");
+Route::get("books/{id}", "BookController@show");
+Route::put("books/{id}", "BookController@update");
+Route::delete("books", "BookController@destroy");
+*/
+
+Route::resource('books', 'BookController');
+
+Route::resource('author', 'AuthorController');
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::get('user-profile', 'AuthController@userProfile');
+});
